a1738bd1b8a35f648efe9a3782c9d006699062fd
YaraFer23 <l21590477@sjuanrio.tecnm.mx>
2025-10-15 20:32:40 -0600

    agregó inicializador
    agregó inicializador

--- Tema2/Naranja/ANIMALES/Inicializador.py
+++ Tema2/Naranja/ANIMALES/Inicializador.py
@@ -1,8 +1,11 @@
-import yaml
+from ruamel.yaml import YAML
+
 
 def inicializador():
+	
     animales = []
     caracteristicas = []
+    yaml = YAML()
 
     print("=== INICIALIZADOR ===")
     print("Ingresa 10 animales:")
@@ -10,7 +13,7 @@ def inicializador():
         animal = input(f"{i+1}. Animal: ").strip()
         animales.append(animal)
 
-    print("\nIngresa 10 características (preguntas binarias como '¿Tiene pelaje?'):")
+    print("\nIngresa 10 características (preguntas de 'si o no':")
     for i in range(10):
         car = input(f"{i+1}. Característica: ").strip()
         caracteristicas.append(car)
@@ -21,7 +24,7 @@ def inicializador():
     }
 
     with open('animales.yaml', 'w') as file:
-        yaml.dump(data, file, default_flow_style=False, allow_unicode=True) 
+        yaml.dump(data, file) 
         
 #El default_flow_stile es para que se guarde de manera mas legible y el unicode permite acentos y ñ	
 	
